90bc2287352fbba2f45f78729d921de5dd990a6e
Qun.Zheng <zhengqun@lingxiaozh.onaliyun.com>
2025-04-08 16:54:33 +0800

    添加更新默认音色的头像和视频URL

--- tests/integration/test_cosyvoice_clone.py
+++ tests/integration/test_cosyvoice_clone.py
@@ -9,7 +9,8 @@ DASHSCOPE_API_KEY = os.getenv("DASHSCOPE_API_KEY")
 
 from dashscope.audio.tts_v2 import VoiceEnrollmentService
 
-sample_url = "http://yixuanhui.oss-cn-shanghai.aliyuncs.com/clone/10s%2B.WAV"
+# sample_url = "http://yixuanhui.oss-cn-shanghai.aliyuncs.com/clone/10s%2B.WAV"
+sample_url = "https://ai-tools66.oss-cn-beijing.aliyuncs.com/uploads/image/1744099834667.wav?Expires=1744103505&OSSAccessKeyId=LTAI5tKmXCiYAFEZsNvDTMCp&Signature=PkDl%2B0U0i%2FpBSI8oOzFeCczdLOU%3D"
 
 voice_service = VoiceEnrollmentService(api_key=DASHSCOPE_API_KEY)
 voice_id = voice_service.create_voice(target_model="cosyvoice-clone-v1",prefix="aihuitu", url=sample_url)
